5bcfc348797f553b97a15e881ae0a4b6d605e47f
RArora28 <rishabharora1997@gmail.com>
2017-06-18 17:07:29 +0530

    add constructor with columns and column_names

--- include/data_frame_variadic.cpp
+++ include/data_frame_variadic.cpp
@@ -4,14 +4,23 @@
 using namespace boost::numeric::ublas;
 
 
-template <class T = void, class ... Args>
+template <class T = void, class ... TArgs>
 class data_frame {
 public:
 	data_frame() {
 		std::cout << typeid(T).name() << std::endl;
 	}
+
+	template <class F1, class F2, class ... FArgs>
+	data_frame(F1 header, vector<F2> col, FArgs ... headers) : 
+		header_(header), col_(col), data_(headers...) {
+		// add conditions for template to be only string.
+		std::cout << typeid(T).name() << ' ' << header_<< std::endl;
+	}
+
+	std::string header_;
 	vector<T> col_;
-	data_frame<Args...> data_;
+	data_frame<TArgs...> data_;
 };
 
 template <>
@@ -24,6 +33,19 @@ public:
 };
 
 int main() {
-	data_frame<int, float, char, std::string> x;
+	vector<int> a(2);
+	a(0) = 1;
+	a(1) = 2;
+	vector<float> b(2);
+	b(0) = 1.1;
+	b(1) = 2.1;
+	vector<char> c(2);
+	c(0) = 'a';
+	c(1) = 'b';
+	vector<std::string> d(2);
+	d(0) = "rishabh";
+	d(1) = "mridul";
+	
+	data_frame<int, float, char, std::string> x("a", a, "b", b, "c", c, "d", d);
 	return 0;
 }
