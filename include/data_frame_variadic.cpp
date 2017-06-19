578756554ef954847b209f63a81db297c2366651
RArora28 <rishabharora1997@gmail.com>
2017-06-19 13:30:06 +0530

    add access operators and functions

--- include/data_frame_variadic.cpp
+++ include/data_frame_variadic.cpp
@@ -3,7 +3,6 @@
 #include <boost/numeric/ublas/vector.hpp>
 using namespace boost::numeric::ublas;
 
-
 template <class T = void, class ... TArgs>
 class data_frame {
 public:
@@ -14,13 +13,31 @@ public:
 	template <class F1, class F2, class ... FArgs>
 	data_frame(F1 header, vector<F2> col, FArgs ... headers) : 
 		header_(header), col_(col), data_(headers...) {
-		// add conditions for template to be only string.
-		std::cout << typeid(T).name() << ' ' << header_<< std::endl;
+		// add conditions for F1 to be only string.
+		end_ = data_.end();
+		std::cout << typeid(T).name() << ' ' << header_ << ' ' << end_ << std::endl;
+	}	
+	
+	// not working: no idea why?! :( 
+	template <class T1> 
+	const vector<T1>& column(const std::string header) const {
+		if (header == header_) {
+			return col_;
+		}
+		else if (end_) {
+			return vector<T1>();
+		}
+		return data_.column<T1>(header);
+	}
+
+	bool end() {
+		return false;
 	}
 
 	std::string header_;
 	vector<T> col_;
 	data_frame<TArgs...> data_;
+	bool end_;
 };
 
 template <>
@@ -28,6 +45,14 @@ class data_frame<> {
 public: 
 	data_frame() {
 		std::cout << "yayyy! end of the recursive class template" << std::endl;
+	}
+	template <class T1> 
+	const vector<T1>& column(const std::string header = "") {
+		vector<T1> x;
+		return x;
+	}
+	bool end() {
+		return true;
 	}
 	// kuch nahi hai abhi yahan pe 
 };
@@ -46,6 +71,9 @@ int main() {
 	d(0) = "rishabh";
 	d(1) = "mridul";
 	
-	data_frame<int, float, char, std::string> x("a", a, "b", b, "c", c, "d", d);
+	data_frame<int, float, char, std::string> df("a", a, "b", b, "c", c, "d", d);
+
+	//vector<int> u = df.column<int>("a") ;
+
 	return 0;
 }
--- include/data_frame_variant.cpp
+++ include/data_frame_variant.cpp
@@ -14,14 +14,6 @@ enum allowed_types {
 	STRING, 
 };
 
-/// \return types of data stored
-template <class T>
-allowed_types Type() {
-	if (std::is_same<T, int>::value)return INT;
-	else if(std::is_same<T, double>::value) return DOUBLE;
-	else if(std::is_same<T, std::string>::value) return STRING;
-}
-
 class data_frame {
 public:
 
@@ -34,27 +26,53 @@ public:
 	data_frame(const size_t col) {
 		col_ = col;
 		header_.resize(col_);
-		types_.resize(col_);
 		row_ = 0;
 	}
 	
 	data_frame(std::vector<std::string> header, std::vector<cumulative_type> data) {
 		// assuming equal sized vectors for now
+		// set conditions for col and row
 		for(size_t i = 0; i < header.size(); i++) {
-			types_.push_back(data.index());
 			header_.push_back(header[i]);
 			data_[header[i]] = data[i];
 		}
 	}
 
+	template <class T>
+	const vector<T>& f(std::string header) {
+		return std::get< vector < T > >(data_[header]);
+	}
+
+	// not working :( : Same thing worked for string version. Fix it fast!!!!
+	const cumulative_type& operator[] (std::string header) const {
+		return data_[header];
+	}
+
+	cumulative_type& operator[] (std::string header) {
+		if (data_.find(header) != data_.end()) {
+			return data_[header];
+		}
+		data_[header];
+		return data_[header];
+	}
 private:
 	size_t col_, row_;
 	std::unordered_map < std::string, cumulative_type > data_;
 	std::vector< std::string > header_;
-	std::vector<allowed_types> types_;
 };
 
 
 int main() {
+	vector<int> x(2);
+	vector<double> y(2);
+	x(0) = 1;
+	x(1) = 2;
+	y(0) = 1.1;
+	y(1) = 2.1;
+
+	cumulative_type X = x, Y = y; 
+	data_frame df({"x", "y"}, {X, Y});
+	vector<double> a = df.f<double>("y");
+	for(int i = 0; i < (int)a.size(); i++) std::cout << a(i) << ' ';
 	return 0;
 }
